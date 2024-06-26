a985085427dbd1dd5185d21e670581683876822f
philyas <lephi@gmx.net>
2024-06-26 13:09:05 +0200

    new fixtures

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -4,10 +4,10 @@ import headerimage from './assets/header.png'
 
 export default function Profile(){
     return(
-        <div id='imageprofile' style={{ width:'100%', height:400, display:'flex', alignItems:'center', textAlign:'center', justifyContent:'center', flexDirection:'column'}}>
+        <div style={{ width:'100%', height:400, display:'flex', alignItems:'center', textAlign:'center', justifyContent:'center', flexDirection:'column'}}>
           
-          <div style={{width:350, height:400, display:'flex', alignItems:'center', justifyContent:'center'}}>
-                    <img width={350} src={headerimage}></img>
+          <div style={{width:300, height:400, display:'flex', alignItems:'center', justifyContent:'center'}}>
+                    <img width={300} src={headerimage}></img>
             </div>
           
             <div style={{width:300, background:'radial-gradient(rgb(255, 255, 255), rgb(223, 231, 236))', borderRadius:1000, height:300, display:'flex', alignItems:'center', justifyContent:'center'}}>
