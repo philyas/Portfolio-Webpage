4b9102938080b798dca1acb5a07b8573ca4f9237
philyas <lephi@gmx.net>
2024-06-27 19:59:17 +0200

    new pp

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,18 +1,19 @@
 import profileimage from './assets/profile.png'
 import headerimage from './assets/header.png'
+import profileimg1 from './assets/profileimg1.png'
 
 
 export default function Profile(){
     return(
         <div style={{ width:'100%', height:'auto', display:'flex', alignItems:'center', textAlign:'center', justifyContent:'center', flexDirection:'column'}}>
           
-          <div style={{ width:300, height:'auto', display:'flex', alignItems:'center', justifyContent:'center'}}>
-                    <img width={300} src={headerimage}></img>
+          <div style={{ width:330, height:'auto', display:'flex', alignItems:'center', justifyContent:'center'}}>
+                    <img style={{background:'red'}} width={330} src={headerimage}></img>
             </div>
           
-            <div style={{width:330, height:330, overflow:'hidden', position:'relative',  borderRadius:1000, display:'flex', alignItems:'center', justifyContent:'center'}}>
-                    <img style={{ position:'absolute', zIndex:2 }} width={330} height={'auto'} src={profileimage}></img>
-                    <div style={{width:280, height:280, marginTop:58, position:'relative', background:'radial-gradient(rgb(255, 255, 255), rgb(223, 231, 236))', borderRadius:1000, display:'flex', alignItems:'center', justifyContent:'center'}}>
+            <div style={{width:330, height:330, overflow:'hidden', position:'relative',  borderRadius:300, display:'flex', alignItems:'center', justifyContent:'center'}}>
+                    <img style={{ position:'absolute', zIndex:2 }} width={290} height={'auto'} src={profileimg1}></img>
+                    <div style={{width:280, height:280, marginTop:70, position:'relative', background:'radial-gradient(rgb(255, 255, 255), rgb(222, 231, 236))', borderRadius:1000, display:'flex', alignItems:'center', justifyContent:'center'}}>
                  
                  </div>
             </div>
